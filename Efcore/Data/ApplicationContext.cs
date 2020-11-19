6984d71ba7f42d43fafa19f26c0163cf8d4fbecc
Thiago Ribeiro Rodrigues <trr.rodrigues@gmail.com>
2020-11-19 06:38:03 -0300

    configura utilização do log de aplicação no ApplicationContext

--- Efcore/Data/ApplicationContext.cs
+++ Efcore/Data/ApplicationContext.cs
@@ -1,16 +1,23 @@
 using Efcore.Data.Configuration;
 using Efcore.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Efcore.Data
 {
     public class ApplicationContext : DbContext
     {
+        private static readonly ILoggerFactory _logger = LoggerFactory.Create(p => p.AddConsole());
         public DbSet<Pedido> Pedido { get; set; }
         public DbSet<Produto> Produto { get; set; }
         public DbSet<Cliente> Cliente { get; set; }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
-            optionsBuilder.UseSqlServer("Data Source=TRR-NOTE;User Id=sa;Password=masterkey;Initial Catalog=EFCore;Integrated Security=false;MultipleActiveResultSets=true;");
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){            
+            
+            optionsBuilder
+                        .UseLoggerFactory(_logger)
+                        .EnableSensitiveDataLogging()
+                        .UseSqlServer("Data Source=TRR-NOTE;User Id=sa;Password=masterkey;Initial Catalog=EFCore;Integrated Security=false;MultipleActiveResultSets=true;");
+                        
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder){
