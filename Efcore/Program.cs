81646c099203f67668d375c1c9323cf6675cb5e7
Thiago Ribeiro Rodrigues <trr.rodrigues@gmail.com>
2020-11-19 06:53:50 -0300

    adiciona metodo para exemplo de inserção em massa

--- Efcore/Program.cs
+++ Efcore/Program.cs
@@ -10,13 +10,13 @@ namespace Efcore
     {
         static void Main(string[] args)
         {
-
             using var db = new Data.ApplicationContext();
             var existe = db.Database.GetPendingMigrations().Any();
             if(existe){
                 
             }
-            InserirDados();                        
+            //InserirDados();                        
+            InserirDadosEmMassa();
         }
         private static void InserirDados(){
             var produto = new Produto{
@@ -37,6 +37,29 @@ namespace Efcore
             var registros = db.SaveChanges();
             Console.WriteLine($"Total de registros Inseridos: {registros}");
 
+        }
+        private static void InserirDadosEmMassa(){
+            var produto = new Produto{
+                  Descricao = "Produto Teste 2",
+                  CodigoBarras = "1232659999",
+                  Valor = 20m,
+                  TipoProduto = TipoProduto.Embalagem,
+                  Ativo = true
+            };
+
+            var cliente = new Cliente {
+                Nome = "Thiago Ribeiro",
+                CEP = "39400000",
+                Cidade = "Montes Claros",
+                Estado = "MG",
+                Telefone = "8798798989"                
+            };
+
+            using var db =  new Data.ApplicationContext();
+            db.AddRange(cliente,produto);
+            var registros = db.SaveChanges();
+            Console.WriteLine($"Total de Registros Inseridos: {registros}");
+        
         }
     }
 }
