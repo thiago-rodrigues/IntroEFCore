a44097fd1d50bd51477a8e69c8fc46daeca0aa26
Thiago Ribeiro Rodrigues <trr.rodrigues@gmail.com>
2020-11-19 06:30:56 -0300

    adiciona método para teste de inserção de dados

--- Efcore/Program.cs
+++ Efcore/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Efcore.Domain;
+using Efcore.ValueObjects;
 
 namespace Efcore
 {
@@ -8,12 +10,33 @@ namespace Efcore
     {
         static void Main(string[] args)
         {
+
             using var db = new Data.ApplicationContext();
             var existe = db.Database.GetPendingMigrations().Any();
             if(existe){
                 
             }
-            Console.WriteLine("Hello World!");
+            InserirDados();                        
+        }
+        private static void InserirDados(){
+            var produto = new Produto{
+                  Descricao = "Produto Teste",
+                  CodigoBarras = "1232654654",
+                  Valor = 10m,
+                  TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                  Ativo = true
+            };
+
+            using var db = new Data.ApplicationContext();
+            
+            db.Produto.Add(produto);
+            db.Set<Produto>().Add(produto);
+            db.Entry(produto).State = EntityState.Added;
+            db.Add(produto);
+
+            var registros = db.SaveChanges();
+            Console.WriteLine($"Total de registros Inseridos: {registros}");
+
         }
     }
 }
